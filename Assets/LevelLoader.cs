c49e5122e45bcc645488f50b92105c9d24451e63
Ross Mawhorter <rmawhorter@g.hmc.edu>
2021-10-20 14:17:18 -0700

    Initial commit of level loading code

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    // Laid out like keyboard keys
+    public Tile Q; // represents platform top right corner
+    public Tile W;
+    public Tile E;
+    public Tile A;
+    public Tile S;
+    public Tile D;
+    public Tile Z;
+    public Tile X;
+    public Tile C;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var tilemap = GetComponentInChildren<Tilemap>();
+        Debug.Log(tilemap.cellBounds);
+        var b = new BoundsInt(-8, -5, 0, 16, 4, 1);
+        MakePlatform(tilemap, b);
+    }
+
+    private void MakePlatform(Tilemap tilemap, BoundsInt bounds)
+    {
+        foreach (var p in bounds.allPositionsWithin)
+        {
+            var tile = GetTile(p, bounds);
+            Debug.Log(p);
+            //Debug.Log(tile);
+            tilemap.SetTile(p, tile);
+        }
+    }
+
+    private Tile GetTile(Vector3Int p, BoundsInt bounds)
+    {
+        var tile = S;
+        // Tiles at the top
+        if (p.y == bounds.yMax - 1)
+        {
+            tile = W;
+            if (p.x == bounds.x)
+            {
+                tile = Q;
+            }
+            if (p.x == bounds.xMax - 1)
+            {
+                tile = E;
+            }
+        }
+        // Tiles at the bottom
+        else if (p.y == bounds.y)
+        {
+            tile = X;
+            if (p.x == bounds.x)
+            {
+                tile = Z;
+            }
+            if (p.x == bounds.xMax - 1)
+            {
+                tile = C;
+            }
+        }
+        // Tiles in the middle
+        else
+        {
+            if (p.x == bounds.x)
+            {
+                tile = A;
+            }
+            if (p.x == bounds.xMax - 1)
+            {
+                tile = D;
+            }
+        }
+        return tile;
+    }
+
+}
