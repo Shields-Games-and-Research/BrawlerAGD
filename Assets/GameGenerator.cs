d6ca1f65a4f7d1b7a5df0dad95103f9db10ac42a
Ross Mawhorter <rmawhorter@g.hmc.edu>
2021-10-22 17:54:06 -0700

    Initial generator with comments.

--- Assets/GameGenerator.cs
+++ Assets/GameGenerator.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class GameGenerator : MonoBehaviour
 {
+
+    public Player player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Generate / Load Map
+
+        // from map, spawnLocation1, spawnLocation2
+        //var spawnLocation1 = new Vector3(0, 0, 0);
+        //var spawnLocation2 = new Vector3(0, 0, 0);
+
+        //Player player1 = Instantiate(player, spawnLocation1); // TODO: location
+        Player player1 = Instantiate(player); // TODO: location
+        //Player player2 = Instantiate(player, spawnLocation2); // location
+
+        // Set keybindings for each player
+
+        // Generate / Load constants for each player
+
+        // Generate / Load constants for the player's moves 
+
     }
 
     // Update is called once per frame
@@ -15,4 +33,9 @@ public class GameGenerator : MonoBehaviour
     {
         
     }
+}
+
+public class MapGenerator
+{
+    //
 }
